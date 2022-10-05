0b443518e582bfcb909deed8cbec61f17eaaaa23
Thavirat Sakeaw <twetheam@gmail.com>
2022-10-05 10:37:28 +0700

    แยกหน้า

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\HomeController;
+use App\Http\Controllers\ProductController;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -13,3 +14,4 @@ use App\Http\Controllers\HomeController;
 |
 */
 Route::resource('/', HomeController::class);
+Route::resource('/product', ProductController::class);
